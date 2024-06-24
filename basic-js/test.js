ee574ed91689eab4c7b31966e106b6bc03004cee
AbhayKumarSingh <139965010+Aks2024@users.noreply.github.com>
2024-06-24 13:01:34 +0000

    basic

--- basic-js/test.js
+++ basic-js/test.js
@@ -1,1 +1,79 @@
-console.log("abhay")
+// console.log("Abhay Kumar Singh")
+
+// vairables and datatypes
+
+const accountId = 1234
+let email = "abhaysingh82476@gmail.com"
+var password = 12345
+accountCity = "Gopalganj"
+
+email = "singhgaurav48036@gmail.com"
+password = 6789
+accoutCity = "Greater Noida"
+
+// console.table([accountId, email, password, accountCity])
+
+// conversion
+
+let marks = 75
+// console.log(typeof marks)
+
+let stringmarks = String(marks)
+// console.log(stringmarks)
+// console.log(typeof(stringmarks))
+
+let score = 0
+
+let originalscore = Boolean(score)
+// console.log(originalscore)
+
+let id = "345ghw"
+let numberid = Number(id)
+// console.log(numberid)
+
+// operations
+
+let value = 100
+value++
+// console.log(value)
+++value
+// console.log(value)
+
+let firstvalue = 100
+firstvalue++
+// console.log(value)
+let secondvalue = 200
+++secondvalue
+// console.log(secondvalue)
+
+// datatypes
+
+const myscore = 100
+const socreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const myid = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 389475973974503n
+
+const heros = ["abhay", "kumar", "singh"];
+let myObj = {
+    name: "Abhay",
+    age: 21,
+}
+
+const myfunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myfunction)
+console.log(typeof heros)
+
+
+
