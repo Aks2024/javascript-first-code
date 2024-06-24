3636a8b4c853d429a94543f6f8dec1aba20e5875
AbhayKumarSingh <139965010+Aks2024@users.noreply.github.com>
2024-06-24 06:35:04 +0000

    datatypes

---
+++ basic-js/variables_01.js
@@ -0,0 +1,10 @@
+const accountId = 1234
+let email = "abhaysingh82476@gmail.com"
+var password = 12345
+accountCity = "Gopalganj"
+
+email = "singhgaurav48036@gmail.com"
+password = 6789
+accoutCity = "Greater Noida"
+
+console.table([accountId, email, password, accountCity])
